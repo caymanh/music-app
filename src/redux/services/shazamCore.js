ba84441b2276a3ba5876e5dfabcb2f94ca0d0164
Cayman Heng <hengcayman@gmail.com>
2023-02-27 22:00:00 +0800

    fixed error preventing Api data from being displayed

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -8,7 +8,7 @@ export const shazamCoreApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://shazam-core.p.rapidapi.com/v1/',
     prepareHeaders: (headers) => {
-        headers.set('X-RapidAPI-Key', '');
+        headers.set('X-RapidAPI-Key', '9a986db254msh3e9a18414377128p1dd856jsn58271c025ce6');
 
         return headers;
     },
@@ -16,7 +16,7 @@ export const shazamCoreApi = createApi({
   //add endpoints property
   endpoints: (builder) => ({
   //build all endpoints of api that will be called
-    getTopCharts: builder.query({ query: './charts/world' }),
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
   }),
 });
 
