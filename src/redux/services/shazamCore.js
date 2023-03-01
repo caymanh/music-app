95f00053a0088f56c67eb7c617cc5d032f1d7e40
Cayman Heng <hengcayman@gmail.com>
2023-03-01 16:31:58 +0800

    added get country API endpoint

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -29,6 +29,9 @@ export const shazamCoreApi = createApi({
     getArtistDetails: builder.query({
       query: (artistId) => `/artists/details?artist_id=${artistId}`,
     }),
+    getSongsByCountry: builder.query({
+      query: (countryCode) => `/charts/country?country_code=${countryCode}`,
+    }),
   }),
 });
 
@@ -37,4 +40,5 @@ export const {
   useGetSongDetailsQuery,
   useGetSongRelatedQuery,
   useGetArtistDetailsQuery,
+  useGetSongsByCountryQuery,
 } = shazamCoreApi;
