9552b198b60672c7de356e18351ae65aa97ae095
Cayman Heng <hengcayman@gmail.com>
2023-03-01 18:14:15 +0800

    updated button position in Song Card component

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,9 +1,11 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
+
 import PlayPause from "./PlayPause";
 import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
-const SongCard = ({ song, isPlaying, activeSong, i, data }) => {
+const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
   const dispatch = useDispatch();
 
   const handlePauseClick = () => {
@@ -19,12 +21,11 @@ const SongCard = ({ song, isPlaying, activeSong, i, data }) => {
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <div className="relative w-full h-56 group">
         <div
-          className={`absolute inet-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
+          className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
             activeSong?.title === song.title
               ? "flex bg-black bg-opacity-70"
               : "hidden"
-          } 
-            `}
+          }`}
         >
           <PlayPause
             isPlaying={isPlaying}
@@ -34,8 +35,13 @@ const SongCard = ({ song, isPlaying, activeSong, i, data }) => {
             handlePlay={handlePlayClick}
           />
         </div>
-        <img alt="song-img" src={song.images?.coverart} />
+        <img
+          alt="song_img"
+          src={song.images?.coverart}
+          className="w-full h-full rounded-lg"
+        />
       </div>
+
       <div className="mt-4 flex flex-col">
         <p className="font-semibold text-lg text-white truncate">
           <Link to={`/songs/${song?.key}`}>{song.title}</Link>
@@ -44,7 +50,7 @@ const SongCard = ({ song, isPlaying, activeSong, i, data }) => {
           <Link
             to={
               song.artists
-                ? `/artists/${song?.artists[0]?.admid}`
+                ? `/artists/${song?.artists[0]?.adamid}`
                 : "/top-artists"
             }
           >
